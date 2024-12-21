09acee528013bb799b52da1564f47186974f822c
PMW9905 <pw9905college@gmail.com>
2024-12-20 21:06:13 -0600

    cleaned up a bit

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,14 +1,26 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  config = function () 
-    local configs = require("nvim-treesitter.configs")
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		local configs = require("nvim-treesitter.configs")
 
-    configs.setup({
-        ensure_installed = {"typescript", "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html", "rust" },
-        sync_install = false,
-        highlight = { enable = true },
-        indent = { enable = true },  
-      })
-  end,
-} 
+		configs.setup({
+			ensure_installed = {
+				"typescript",
+				"c",
+				"lua",
+				"vim",
+				"vimdoc",
+				"query",
+				"elixir",
+				"heex",
+				"javascript",
+				"html",
+				"rust",
+			},
+			sync_install = false,
+			highlight = { enable = true },
+			indent = { enable = true },
+		})
+	end,
+}
