41bd6a3d31d94ba23c87966cc5d650e52aa48d79
PMW9905 <pw9905college@gmail.com>
2024-12-20 19:43:14 -0600

    Removed requirement for eslint

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -11,8 +11,6 @@ return {
       require("mason-lspconfig").setup({
         ensure_installed = {
           "lua_ls",
-          "rust_analyzer",
-          "eslint"
         }
       })
     end
@@ -22,8 +20,6 @@ return {
     config = function()
       local lspconfig = require("lspconfig")
       lspconfig.lua_ls.setup({})
-      lspconfig.rust_analyzer.setup({})
-      lspconfig.eslint.setup({})
 
       vim.keymap.set('n', '<leader>hd', vim.lsp.buf.hover, {})
       vim.keymap.set('n', '<leader>gd', vim.lsp.buf.definition, {})
