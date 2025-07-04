926aa9ffb83e5a961e3b9902138d89fccab11a20
Parker Whitehead <pmw9905@gmail.com>
2025-07-04 14:38:53 -0500

    tab is now completion character

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -1,80 +1,80 @@
 return {
-	{
-		"hrsh7th/cmp-nvim-lsp",
-	},
-	{
-		"L3MON4D3/LuaSnip",
-		dependencies = {
-			"saadparwaiz1/cmp_luasnip",
-			"rafamadriz/friendly-snippets",
-		},
-	},
-	{
-		"hrsh7th/nvim-cmp",
-		config = function()
-			local cmp = require("cmp")
-			require("luasnip.loaders.from_vscode").lazy_load()
-			cmp.setup({
-				snippet = {
-					expand = function(args)
-						require("luasnip").lsp_expand(args.body)
-					end,
-				},
-				completion = {
-					keyword_length = 1,
-					keyword_pattern = [[\k\+]],
-					ignore_case = true,
-				},
-				matching = {
-					disallow_fuzzy_matching = false,
-					disallow_partial_matching = false,
-					disallow_prefix_unmatching = false,
-				},
-				window = {
-					completion = cmp.config.window.bordered(),
-					documentation = cmp.config.window.bordered(),
-				},
-				mapping = cmp.mapping.preset.insert({
-					["<C-b>"] = cmp.mapping.scroll_docs(-4),
-					["<C-f>"] = cmp.mapping.scroll_docs(4),
-					["<C-Space>"] = cmp.mapping.complete(),
-					["<C-e>"] = cmp.mapping.abort(),
-					["<CR>"] = cmp.mapping.confirm({ select = true }),
-				}),
-				sources = cmp.config.sources({
-					{ name = "nvim_lsp", priority = 1000 },
-					{ name = "luasnip", priority = 750 },
-					{
-						name = "buffer",
-						priority = 500,
-						keyword_length = 1,  -- Changed from 3 to 1 for faster suggestions
-						max_item_count = 5,
-						option = {
-							get_bufnrs = function()
-								return { vim.api.nvim_get_current_buf() }
-							end,
-							indexing_threshold = 1000,
-							indexing_interval = 100,
-							ignore_case = true,
-							context_pattern = [[\k\+]],
-						},
-					},
-				}),
-				sorting = {
-					priority_weight = 2,
-					comparators = {
-						cmp.config.compare.offset,
-						cmp.config.compare.exact,
-						cmp.config.compare.score,
-						cmp.config.compare.recently_used,
-						cmp.config.compare.locality,
-						cmp.config.compare.kind,
-						cmp.config.compare.sort_text,
-						cmp.config.compare.length,
-						cmp.config.compare.order,
-					},
-				},
-			})
-		end,
-	},
+  {
+    "hrsh7th/cmp-nvim-lsp",
+  },
+  {
+    "L3MON4D3/LuaSnip",
+    dependencies = {
+      "saadparwaiz1/cmp_luasnip",
+      "rafamadriz/friendly-snippets",
+    },
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    config = function()
+      local cmp = require("cmp")
+      require("luasnip.loaders.from_vscode").lazy_load()
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            require("luasnip").lsp_expand(args.body)
+          end,
+        },
+        completion = {
+          keyword_length = 1,
+          keyword_pattern = [[\k\+]],
+          ignore_case = true,
+        },
+        matching = {
+          disallow_fuzzy_matching = false,
+          disallow_partial_matching = false,
+          disallow_prefix_unmatching = false,
+        },
+        window = {
+          completion = cmp.config.window.bordered(),
+          documentation = cmp.config.window.bordered(),
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-f>"] = cmp.mapping.scroll_docs(4),
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<C-e>"] = cmp.mapping.abort(),
+          ["<Tab>"] = cmp.mapping.confirm({ select = true }),
+        }),
+        sources = cmp.config.sources({
+          { name = "nvim_lsp", priority = 1000 },
+          { name = "luasnip",  priority = 750 },
+          {
+            name = "buffer",
+            priority = 500,
+            keyword_length = 1, -- Changed from 3 to 1 for faster suggestions
+            max_item_count = 5,
+            option = {
+              get_bufnrs = function()
+                return { vim.api.nvim_get_current_buf() }
+              end,
+              indexing_threshold = 1000,
+              indexing_interval = 100,
+              ignore_case = true,
+              context_pattern = [[\k\+]],
+            },
+          },
+        }),
+        sorting = {
+          priority_weight = 2,
+          comparators = {
+            cmp.config.compare.offset,
+            cmp.config.compare.exact,
+            cmp.config.compare.score,
+            cmp.config.compare.recently_used,
+            cmp.config.compare.locality,
+            cmp.config.compare.kind,
+            cmp.config.compare.sort_text,
+            cmp.config.compare.length,
+            cmp.config.compare.order,
+          },
+        },
+      })
+    end,
+  },
 }
